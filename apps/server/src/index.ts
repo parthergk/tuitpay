fa026bbfabe172f0eca7c64fd9accf5ad2053a20
parthergk <gauravkumar81464@gmail.com>
2025-06-29 16:03:52 +0530

    added routes

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
-const app = express();
+import cors from "cors";
 
+const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(PORT,()=>{
     console.log(`https server is runing on port ${PORT}`);
