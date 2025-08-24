1bbf4a331373f686de294c767847abf275a56406
parthergk <gauravkumar81464@gmail.com>
2025-08-24 17:24:16 +0530

    added the Testimonials section

--- apps/web/app/(root)/(Home)/page.tsx
+++ apps/web/app/(root)/(Home)/page.tsx
@@ -10,6 +10,7 @@ import Link from "next/link";
 import DashboardCard from "../../../components/LandingPage/DashboardCard";
 import Image from "next/image";
 import FeatureCard from "../../../components/LandingPage/FeatureCard";
+import TestimonialsCard from "../../../components/LandingPage/TestimonialsCard";
 
 interface Inputs {
   email: string;
@@ -133,6 +134,7 @@ export default function Home() {
           <div className="absolute w-full h-80 bg-gradient-to-t from-[#EAE2FF] via-30% via-[#EAE2FF]/90 to-transparent -bottom-5"></div>
         </div>
       </section>
+
       <section className="mt-5 w-ful py-14 md:py-28 px-5">
         <div className="w-full mt-5 flex flex-col justify-center gap-2 md:gap-4">
           <h1 className=" text-center sm:text-lg md:text-xl text-[#F97316]">
@@ -150,6 +152,7 @@ export default function Home() {
           </div>
         </div>
       </section>
+
       <section className="mt-5 w-full py-14 md:py-28 px-5">
         <h3 className=" text-center sm:text-lg md:text-xl text-[#F97316]">
           FEATURES
@@ -186,8 +189,24 @@ export default function Home() {
             </div>
           </div>
           <div className=" relative shadow-2xl shadow-black/10 border border-white/50 rounded-xl">
-              <div className="h-full w-full absolute opacity-25 md:opacity-35 [background-image:linear-gradient(to_right_top,_#FFFFFF_0%,_#E0ECFF_25%,_#EAE2FF_50%,_#F8E8DB_75%,_#FFFFFF_100%)]"></div>
-            
+            <div className="h-full w-full absolute opacity-25 md:opacity-35 [background-image:linear-gradient(to_right_top,_#FFFFFF_0%,_#E0ECFF_25%,_#EAE2FF_50%,_#F8E8DB_75%,_#FFFFFF_100%)]"></div>
+          </div>
+        </div>
+      </section>
+
+      <section className="mt-5 w-full py-14 md:py-28 px-5">
+        <h3 className=" text-center sm:text-lg md:text-xl text-[#F97316]">
+          TESTIMONIALS
+        </h3>
+        <h1 className=" text-center text-[28px] sm:text-4xl mt-5 ">
+          What Our Users Say
+        </h1>
+        <div className="w-full relative pt-16 overflow-x-hidden lg:mt-[72px]">
+          <div className="absolute inset-0 bg-gradient-to-l from-[#EAE2FF] via-transparent to-[#EAE2FF]"></div>
+          <div className="grid grid-cols-3 gap-6 min-w-max">
+            {new Array(1, 2, 3, 4, 5, 6).map((item) => (
+              <TestimonialsCard key={item} />
+            ))}
           </div>
         </div>
       </section>
---
+++ apps/web/components/LandingPage/TestimonialsCard.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const TestimonialsCard = () => {
+  return (
+    <div className=" w-[300px] md:w-[419px]  p-3 bg-gradient-to-bl from-[#E8DFFF]/30 to-[#DDEBFF]/30 shadow-2xl shadow-black/10 border border-white/50 rounded-xl">
+      <p className="text-sm sm:text-base leading-snug text-[#4B5563]">
+        Easily track every student in one place. Add new students, update
+        details, and manage without any hassle. With our intuitive student
+        database, teachers can save time and focus more on teaching.
+      </p>
+      <div className=" flex items-center gap-5 mt-3 sm:mt-5">
+        <div className=" bg-gray-500 h-12 w-12 sm:h-15 sm:w-15 rounded-full"></div>
+        <div className=" flex flex-col space-y-0.5 sm:space-y-1">
+          <h3 className=" text-lg md:text-xl">Jatin Sharma</h3>
+          <span className="text-[#4B5563]">Math</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialsCard;
