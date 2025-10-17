01fa0336d8882d51fa1f2f52beff884b4cab09c0
parthergk <gauravkumar81464@gmail.com>
2025-10-18 01:25:20 +0530

    added the api call for all students

--- apps/web/components/dashboard/Students.tsx
+++ apps/web/components/dashboard/Students.tsx
@@ -1,22 +1,11 @@
 import { BadgeCheck, Pen, Trash2 } from "lucide-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Students = () => {
   const [showForm, setShowForm] = useState(false);
   const handleAddStudent = () => {
     setShowForm((prev) => !prev);
   };
-
-  const items = [
-    "Name",
-    "Course",
-    "Monthly Fee",
-    "Due Date",
-    "Join Date",
-    "Status",
-    "Actions",
-  ];
-
   const [students, setStudents] = useState([
     {
       id: 1,
@@ -46,8 +35,37 @@ const Students = () => {
       status: "Pending",
     },
   ]);
+
+  async function fetchStudents() {
+    const response = await fetch("http://localhost:8080/api/v1/student", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    const result = await response.json();
+    // setStudents(result.students);
+    console.log("Students", result.students);
+    
+  }
+
+  useEffect(()=>{
+    fetchStudents();
+  },[]);
+
+  const items = [
+    "Name",
+    "Course",
+    "Monthly Fee",
+    "Due Date",
+    "Join Date",
+    "Status",
+    "Actions",
+  ];
+  
   return (
-    <div>
+    <>
       <div className=" flex justify-between items-center">
         <button className=" bg-primary text-white py-1 px-2.5 sm:py-1.5 sm:px-3 rounded-md shadow-md cursor-pointer">
           Add Student
@@ -61,7 +79,7 @@ const Students = () => {
 
       <div className=" w-full mt-8 overflow-x-auto min-h-80 shadow-lg  border border-white/50 rounded-lg">
         <div className=" w-full min-w-[800px] md:min-w-[590px] bg-primary text-white  px-4 py-3 rounded-tl-lg rounded-se-xl shadow-lg">
-          <ul className=" grid grid-cols-7">
+          <ul className=" grid grid-cols-7 gap-1">
             {items.map((item) => (
               <li key={item}>{item}</li>
             ))}
@@ -70,45 +88,50 @@ const Students = () => {
 
         <div className=" w-full p-4 min-w-[800px] md:min-w-[590px]">
           <ul className="space-y-3">
-            {students.length > 0 ?  students.map((student) => (
-              <li key={student.id} className=" w-full grid grid-cols-7  items-center border-b border-slate-100 pb-2 text-sm sm:text-base">
-                <span className="truncate">{student.name}</span>
-                <span className="truncate">{student.course}</span>
-                <span>{student.fee}</span>
-                <span>{student.dueDate}</span>
-                <span>{student.joinDate}</span>
-                <div
-                  className={` w-full max-w-20 sm:max-w-24 px-2 py-0.5 rounded-2xl text-center border ${
-                    student.status.toLowerCase() === "paid"
-                      ? "border-green-500 text-green-600"
-                      : student.status.toLowerCase() === "pending"
-                        ? "border-yellow-500 text-yellow-600"
-                        : "border-red-500 text-red-600"
-                  }`}
+            {students.length > 0 ? (
+              students.map((student) => (
+                <li
+                  key={student._id}
+                  className=" w-full grid grid-cols-7 gap-1 items-center border-b border-slate-100 pb-2 text-sm sm:text-base"
                 >
-                  {student.status}
-                </div>
-                <div className="flex gap-5">
-                  <button className=" text-sub hover:underline text-sm cursor-pointer">
-                    <Pen className=" h-4 w-4"/>
-                  </button>
-                  <button className="text-red-600 hover:underline text-sm cursor-pointer">
-                    <Trash2 className=" h-4 w-4"/>
-                  </button>
-                  <button className=" text-sub hover:underline text-sm cursor-pointer">
-                    <BadgeCheck className=" h-4 w-4"/>
-                  </button>
-                </div>
-              </li>
-            )): (
-            <p className="text-center text-gray-500 py-4">
-              No students found.
-            </p>
-          )}
+                  <span className="truncate">{student.name}</span>
+                  <span className="truncate">{student.sub}</span>
+                  <span>₹{student.monthlyFee}</span>
+                  <span>{student.feeDay}</span>
+                  <span>{student.joinDate}</span>
+                  <div
+                    className={` w-full max-w-20 sm:max-w-24 px-2 py-0.5 rounded-2xl text-center border ${
+                      student.status.toLowerCase() === "paid"
+                        ? "border-green-500 text-green-600"
+                        : student.status.toLowerCase() === "pending"
+                          ? "border-yellow-500 text-yellow-600"
+                          : "border-red-500 text-red-600"
+                    }`}
+                  >
+                    {student.status}
+                  </div>
+                  <div className="flex gap-5">
+                    <button className=" text-sub hover:underline text-sm cursor-pointer">
+                      <Pen className=" h-4 w-4" />
+                    </button>
+                    <button className="text-red-600 hover:underline text-sm cursor-pointer">
+                      <Trash2 className=" h-4 w-4" />
+                    </button>
+                    <button className=" text-sub hover:underline text-sm cursor-pointer">
+                      <BadgeCheck className=" h-4 w-4" />
+                    </button>
+                  </div>
+                </li>
+              ))
+            ) : (
+              <p className="text-center text-gray-500 py-4">
+                No students found.
+              </p>
+            )}
           </ul>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
