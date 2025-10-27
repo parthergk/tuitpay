9ae07e0d190235e26bf9d35d1d13f3857fa2b343
parthergk <gauravkumar81464@gmail.com>
2025-10-27 13:20:52 +0530

    fix the key error and height for small devices in feetracking section

--- apps/web/components/dashboard/FeeTracking.tsx
+++ apps/web/components/dashboard/FeeTracking.tsx
@@ -114,9 +114,9 @@ const FeeTracking = () => {
         className="py-1 px-2.5 sm:py-1.5 sm:px-3 border border-slate-300 rounded-md focus:outline-none focus:ring-1 focus:ring-primary focus:border-primary"
       />
       <div className=" w-full mt-8 overflow-x-auto min-h-80 shadow-lg border border-white/50 rounded-lg">
-        <div className=" w-full h-full p-4 min-w-[810px] md:min-w-[600px] max-h-80 overflow-y-scroll space-y-3">
-          {searchData.map((student) => (
-            <table className="w-full border-collapse text-sm">
+        <div className=" w-full h-full p-4 min-w-[810px] md:min-w-[600px] sm:max-h-80 overflow-y-scroll space-y-3">
+          {searchData.map((student, i) => (
+            <table key={i} className="w-full border-collapse text-sm">
               <caption className="text-lg font-semibold mb-1 text-heading text-start">
                 {student.name}
               </caption>
