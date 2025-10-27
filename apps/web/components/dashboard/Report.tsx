81d13a695a30a5967ecfbbca18adda92dca7367f
parthergk <gauravkumar81464@gmail.com>
2025-10-27 13:16:05 +0530

    added chart for monthly collection

--- apps/web/components/dashboard/Report.tsx
+++ apps/web/components/dashboard/Report.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 import CollectionCard from "./CollectionCard";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 const Report = () => {
   const reportData = {
@@ -9,12 +18,6 @@ const Report = () => {
       activeStudents: 10,
       pendingOutstanding: 2400,
     },
-    summary: [
-      { thisMonthCollection: 4800 },
-      { totalRevenue: 45200 },
-      { activeStudents: 10 },
-      { pendingOutstanding: 2400 },
-    ],
     monthlyRevenue: [
       { month: "June", collected: 6200 },
       { month: "July", collected: 7400 },
@@ -23,16 +26,35 @@ const Report = () => {
       { month: "October", collected: 4800 },
     ],
   };
+
   return (
-    <>
-      <div className="flex flex-col px-4 py-2 shadow-lg shadow-black/10 border border-white/50 rounded-xl min-h-[250px]">
-        {/* header */}
-        <h2 className=" border-b pb-2 text-xl text-heading">
+    <div className=" w-full">
+      <div className="flex flex-col py-2">
+        <h2 className="border-b pb-2 text-xl text-heading">
           Total Collection Report
         </h2>
-        <CollectionCard summaryStats={reportData.summaryStats}/>
+        <CollectionCard summaryStats={reportData.summaryStats} />
       </div>
-    </>
+        <ResponsiveContainer  className="text-xs md:text-sm lg:text-base leading-snug" width="100%" height={200}>
+          <LineChart
+            data={reportData.monthlyRevenue}
+            margin={{ top: 10, right: 20, left: -10, bottom: 0 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" stroke="#e5e7eb" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Line
+              type="monotone"
+              dataKey="collected"
+              stroke="#F97316"
+              strokeWidth={1}
+              dot={{ r: 5 }}
+              activeDot={{ r: 8 }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+    </div>
   );
 };
 
